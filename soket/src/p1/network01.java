94956effaeca2953618f4fc5e206dec33ad001c1
kwonkihun <rlgns123456@naver.com>
2017-05-02 13:18:45 +0900

    Update network01.java

--- soket/src/p1/network01.java
+++ soket/src/p1/network01.java
@@ -11,36 +11,7 @@ public class network01 {
 
 	public static void main(String[] args) throws IOException {
 
-		ServerSocket serverSocket = null;	//서버
-		Socket clientSocket = null;
-		PrintWriter out = null;
-		BufferedReader in = null;
-
-		serverSocket = new ServerSocket(80);
-
-		try {
-			System.out.println("start");
-			clientSocket = serverSocket.accept();
-			System.out.println("클라이언트 접속");
-			//out = new PrintWriter(clientSocket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-			while(true) {
-				String inputLine = null;
-				inputLine = in.readLine();
-				System.out.println("클라이언트로부터 받은 문자 : " + inputLine);
-				//out.println(inputLine);	//어플으로 다시 보내주는 부분
-
-				if(inputLine.equals("exit"))
-					break;
-			}
-			//out.close();
-			in.close();
-			clientSocket.close();
-			serverSocket.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 
